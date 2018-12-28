9dd4fd6d5724b5abaf154c4bc2221e406eb43c79
Daniel Martin <daniel@danielDevelops.com>
2018-12-28 16:19:17 -0500

    initial commit

---
+++ FaceTheWeather/src/components/Weather/index.js
@@ -0,0 +1,15 @@
+//@flow
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+type Props = {
+
+}
+type State = {
+
+}
+
+export default class Weather extends Component<Props,State>{
+
+}

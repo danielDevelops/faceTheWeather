6be055b951e107ed05d16e57409df31162428b6c
Daniel Martin <daniel@danielDevelops.com>
2019-03-29 22:58:33 -0400

    updated presentation

---
+++ app.config.js
@@ -0,0 +1,22 @@
+// @flow
+
+//This is a sample app.config but these are required to use this application
+
+const config = {
+    darkSkyConfig:{
+        url:'https://api.darksky.net',
+        secret:''
+    },
+    azureCognativeService:{
+        uri:'https://eastus.api.cognitive.microsoft.com',
+        key:''
+    },
+    azureMapService:{
+        uri:'https://atlas.microsoft.com',
+        key:''
+    }
+
+
+
+}
+export default config;
